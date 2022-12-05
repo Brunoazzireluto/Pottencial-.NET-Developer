124b66d21691e8cac8e9fa4bdd71efcffb934632
Bruno Azzireluto <bruno.a@outlook.com.br>
2022-12-05 20:08:42 -0300

    Adding type of data

---
+++ Tipos de Dados/Program.cs
@@ -0,0 +1,15 @@
+﻿string apresentacao = "Olá Seja bem vindo";
+
+int quantidade = 2;
+
+double altura = 1.80;
+
+decimal preco = 1.88M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da Variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da Variavel altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da Variavel preço: " + preco);
+Console.WriteLine("Valor da Variavel condição: " + condicao);
