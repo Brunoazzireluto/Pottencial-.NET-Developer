1e3449384a9d5685ad8a2662876c78f6e89e082a
azzireluto <bruno.a@outlook.com.br>
2022-12-06 17:19:53 -0300

    Adding new files

---
+++ Operadores de Atribuição/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 5;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+// c = c + 5;
+c += 5;
+Console.WriteLine(c);
+
+// c = c - 6;
+
+c -= 6;
+Console.WriteLine(c);
+
+
+// Convertendo Tipos de Variaveis
+//Cast - Casting
+int d = Convert.ToInt32("5");
+Console.WriteLine(d);
+int e = int.Parse("5"); // -> Todos os tipos terão o método Parse
+Console.WriteLine(e);
+
--- Tipos de Dados/Program.cs
+++ Tipos de Dados/Program.cs
@@ -13,3 +13,20 @@ Console.WriteLine("Valor da Variavel quantidade: " + quantidade);
 Console.WriteLine("Valor da Variavel altura: " + altura.ToString("0.00"));
 Console.WriteLine("Valor da Variavel preço: " + preco);
 Console.WriteLine("Valor da Variavel condição: " + condicao);
+
+quantidade = 10;
+
+Console.WriteLine("O novo valor da Variavel quantidade é: " + quantidade);
+
+
+//Variavel de Data
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+DateTime dataMaisCinco =  dataAtual.AddDays(5);
+
+Console.WriteLine(dataMaisCinco);
+
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
